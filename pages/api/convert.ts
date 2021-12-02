2cfff105c48761b322622de55a327536f4e8f972
Dennis Keil <dennis.keil10@gmail.com>
2021-12-02 12:19:12 +0100

    Update

--- pages/api/convert.ts
+++ pages/api/convert.ts
@@ -1,15 +1,33 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from 'axios';
+import YAML from 'yaml'
 
 type Data = {
   name: string;
   url: string;
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   const url = req.query.url as string;
-  res.status(200).json({ name: 'John Doe', url })
+
+  const response = await axios({
+    method: 'GET',
+    url,
+  });
+
+  const result = YAML.parse(response.data);
+
+  res.status(200).json(result);
 }
+
+// export default function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<Data>
+// ) {
+//   const url = req.query.url as string;
+//   res.status(200).json({ name: 'John Doe', url })
+// }
